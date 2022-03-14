ad3c22b2e38bfb7afd76d4d5c113df8b472ba8d0
emilie dorer <emiliedorer@gmail.com>
2022-03-14 13:48:40 +0100

    recuperation of id

--- test_api.sh
+++ test_api.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 URL="http://localhost/Dodos/"
 page=$1
-URL_page=$URL$page"/"
+URL_page='"'$URL$page'/"'
 echo $URL_page
-id= "curl 'http://localhost/Dodos/?rest_route=/wp/v2/pages/' | jq '.[] | select(.link=='$URL_page') | .id'"
+id=`curl 'http://localhost/Dodos/?rest_route=/wp/v2/pages/' | jq '.[] | select(.link=='$URL_page') | .id'`
 echo $id
