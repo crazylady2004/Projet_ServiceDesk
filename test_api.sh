4ed5e09ac96f23c12bf8e3d999a48f44387e251c
emilie dorer <emiliedorer@gmail.com>
2022-03-14 08:29:20 +0100

    creation variable id with curl

--- test_api.sh
+++ test_api.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-URL="https://localhost/Dodos/"
-read page
-URL_page=$URL$page
+URL="http://localhost/Dodos/"
+page=$1
+URL_page=$URL$page"/"
 echo $URL_page
+id= "curl 'http://localhost/Dodos/?rest_route=/wp/v2/pages/' | jq '.[] | select(.link=='$URL_page') | .id'"
+echo $id
