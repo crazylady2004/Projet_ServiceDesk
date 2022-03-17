a78433321ab47f1e919abb4bb0048d5752537a44
emilie dorer <emiliedorer@gmail.com>
2022-03-17 12:17:59 +0100

    recuperation path root of url

--- test_api.sh
+++ test_api.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-URL="http://localhost/Dodos/"
+URL="http://localhost/Dodos/index.php/"
 page=$1
 URL_page='"'$URL$page'/"'
 echo $URL_page
 id=`curl 'http://localhost/Dodos/?rest_route=/wp/v2/pages/' | jq '.[] | select(.link=='$URL_page') | .id'`
 echo $id
+
+
+json="?rest_route=/wp/v2/"
+URL_json=$1$json
+test=`curl $URL_json | jq '._links.up | .[0].href'`
+echo $test;
