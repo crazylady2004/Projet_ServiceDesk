bb6ee1e3f88e2e0a9a107acd5426d7909fb133c0
emilie dorer <emiliedorer@gmail.com>
2022-04-27 14:04:35 +0200

    finish bash

--- test_api.sh
+++ test_api.sh
@@ -8,22 +8,12 @@ original_url=`curl $URL_json | jq '._links.up | .[0].href'`
 racine_url=$(echo $original_url | awk '{ print substr( $0, 1, length($0)-9 ) }' | sed 's/^.//')
 
 # récuperation de l'id
-id=`curl $racine_url'wp-json/wp/v2/pages/' | jq '.[] | select(.link=="'$URL'") | .id'`
-echo $id
-
-#nom de la page
-name_page=`curl $racine_url'wp-json/wp/v2/pages/'$id | jq '.slug'`
-echo $name_page
+id=`curl $racine_url'wp-json/wp/v2/pages/?per_page=20' | jq '.[] | select(.link=="'$URL'") | .id'`
 
 #récup. date_modified / ID_Users
-# Tab_wp=`wp db query --skip-column-names "SELECT post_author, post_modified FROM wp_posts WHERE post_parent=77 and post_type='revision' ORDER by post_date limit 1"`
+requet_tab=`wp db query --skip-column-names "SELECT post_author, post_modified FROM wp_posts WHERE post_parent=$id and post_type='revision' ORDER by post_date desc limit 1"`
+Tableau=($requet_tab)
 
-# # récuperation de l'id_user
-# id_user=`curl $sans_json'wp-json/wp/v2/' | jq '.[] | select(.link=="'$1'") | .author'`
-
-# # récuperation du user_name
-# User_name=`curl $1'wp-json/wp/v2/'$id_user | jq '.name'`
-# echo $User_name
-
-#  curl $sans_json'wp-json/wp/v2/pages' | jq '.[] | select(.link=="https://migration-wp.epfl.ch/emilie/education-fr/sous-page-fr-3/")'
-#  curl $sans_json'wp-json/wp/v2/pages' | jq '.[] | select(.link=="https://migration-wp.epfl.ch/emilie/en/education-en/sous-page-en-1/")'  
+# récuperation du user_name
+# "curl https://migration-wp.epfl.ch/emilie/wp-json/wp/v2/users/3 | jq '.name'"
+user_name=`curl $racine_url'wp-json/wp/v2/users/'${Tableau[0]} | jq '.name'`
