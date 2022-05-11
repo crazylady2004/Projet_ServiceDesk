16d448e2d818c4cf814b5a7e66dd0889de0b49f4
emilie dorer <emiliedorer@gmail.com>
2022-05-11 14:10:06 +0200

    correction and add title

--- emilie-function.php
+++ emilie-function.php
@@ -1,13 +1,13 @@
 <?php
   
 add_action( 'rest_api_init', function() {
-        register_rest_route( 'test/v2', '/toto', [
+        register_rest_route( 'test/v2', 'toto', array (
                 'method'   => WP_REST_Server::READABLE,
-                'callback' => 'awhitepixel_rest_route_getsomedata',
-        ] );
+                'callback' => 'awhitepixel_rest_route_getsomedata'
+        ) );
 } );
-function awhitepixel_rest_route_getsomedata( $request ) {
- $name = $request->get_param('name');
+function awhitepixel_rest_route_getsomedata($post_ID) {
+        $titre = get_the_title(135);
+        return rest_ensure_response ($titre);
 
-        return rest_ensure_response("hello {$name}!");
 }
