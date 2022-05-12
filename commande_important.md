3380c570c8dcd127730a2d125d2bda3b7821b092
emilie dorer <emiliedorer@gmail.com>
2022-05-12 14:21:39 +0200

    add modified date and change name url

---
+++ commande_important.md
@@ -0,0 +1,4 @@
+commande important
+
+docker run -p "80:80" -v ${PWD}/app:/app -v ${PWD}/db:/var/lib/mysql mattrayner/lamp:latest
+/srv/int/migration-wp.epfl.ch/htdocs/emilie/wp-content/mu-plugins
--- emilie-function.php
+++ emilie-function.php
@@ -1,15 +1,17 @@
 <?php
-  
+
 add_action( 'rest_api_init', function() {
-        register_rest_route( 'test/v2', 'toto/(?<id>.*)', array (
+        register_rest_route( 'revisions/v2', 'epfl-funciton/(?<id>.*)', array (
                 'method'   => WP_REST_Server::READABLE,
                 'callback' => 'get_test'
         ) );
 } );
 function get_test($request) {
         $titre = get_the_title($request['id']);
+        $modif = get_the_modified_date('Y-m-d H:i:s',$request['id']);
+        $user = get_the_modified_author( $request['id']);
 
-        return rest_ensure_response ($titre);
+        return rest_ensure_response ($user -> display_name);
 
 
 }
